999353f8af7acbcc810b167eaa586ac3b414beae
lucas-im <lucas@hypercluster.page>
2022-02-28 12:26:52 +0900

    cleanup

---
+++ tampermonkey/tm-netflix.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         netflix
+// @namespace    https://netflix.com/*
+// @version      0.1
+// @description  netflix helper
+// @author       me
+// @match        https://www.netflix.com/*
+// @icon         https://www.google.com/s2/favicons?domain=netflix.com
+// @grant        none
+// @run-at       document-end
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+
+
+if($('.profile-gate-label')[0] !== null) {
+    $('.profile-icon')[0].click()
+
+}
+
+const observer = new MutationObserver((_) => {
+})
+const config = {
+    childList: true,
+    subtree: true
+}
+observer.observe(document, config);
--- win_startup.py
+++ win_startup.py
@@ -1,13 +1,11 @@
 import win32gui, pyautogui, time, win32con
 
-for i in range(3):
-    window = win32gui.FindWindow(None, 'Inbox - <EMAIL> - Outlook')
-    res = win32gui.ShowWindow(window, win32con.SW_MINIMIZE)
-    print(res)
-    if res == 0:
-        time.sleep(2.5)
-    else:
-        pyautogui.keyDown('winleft')
-        pyautogui.press('space')
-        pyautogui.keyUp('winleft')
-        break
+time.sleep(2)
+window = win32gui.FindWindow(None, 'Inbox - lucas.1m@icloud.com - Outlook')
+win32gui.ShowWindow(window, win32con.SW_MINIMIZE)
+time.sleep(6)
+win32gui.ShowWindow(window, win32con.SW_NORMAL)
+win32gui.ShowWindow(window, win32con.SW_MINIMIZE)
+#pyautogui.keyDown('winleft')
+#pyautogui.press('space')
+#pyautogui.keyUp('winleft')
