fa3e8cd2b6632154d4003a114c0177592aad618d
lucas-im <lucas@hypercluster.page>
2022-03-01 18:30:31 +0900

    color indicators for priv/pub repo

--- tampermonkey/tm-dangerclose.js
+++ tampermonkey/tm-dangerclose.js
@@ -6,19 +6,22 @@
 // @author       me
 // @match        https://github.com/*
 // @icon         https://www.google.com/s2/favicons?domain=github.com
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @grant        none
+// @run-at       document-end
 // ==/UserScript==
 
 
 
 const observer = new MutationObserver((_) => {
+    if (document.URL.includes('=repositories')) {
+        $('.Label--secondary').each((_, el) => {
+            if (el.innerText === 'Public') el.style.background = '#451313'
+            else el.style.background = '#003008'
+        })
+    }
     if (document.querySelector('#settings-tab').classList.contains('selected')) {
-        if (document.URL.includes('=repositories')) {
-            $('.Label--secondary').each((_, el) => {
-                if (el.innerText === 'Public') el.style.background = '#451313'
-                else el.style.background = '#003008'
-            })
-        }
+
         let inputElements = document.querySelectorAll('.form-control.input-block');
         if (inputElements.length > 0) {
             const url = document.URL.split('/')
